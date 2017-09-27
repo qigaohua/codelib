1f2b293a7f84283bb94e96016ba7623fb825268b
qigaohua <qigaohua168@163.com>
2017-09-27 03:34:52 -0700

    edit

--- socket.c
+++ socket.c
@@ -42,3 +42,118 @@ int Socket(const char *host, int clientPort)
     return sock;
 }
 
+/*通过网络接口（如eth1）获取 mac地址
+ *device: eth1
+ *buf:  mac
+ 
+struct ifreq {
+#define IFHWADDRLEN	6
+	union
+	{
+		char	ifrn_name[IFNAMSIZ];		// if name, e.g. "en0" 
+	} ifr_ifrn;
+	
+	union {
+		struct	sockaddr ifru_addr;
+		struct	sockaddr ifru_dstaddr;
+		struct	sockaddr ifru_broadaddr;
+		struct	sockaddr ifru_netmask;
+		struct  sockaddr ifru_hwaddr;
+		short	ifru_flags;
+		int	ifru_ivalue;
+		int	ifru_mtu;
+		struct  ifmap ifru_map;
+		char	ifru_slave[IFNAMSIZ];	// Just fits the size 
+		char	ifru_newname[IFNAMSIZ];
+		void __user *	ifru_data;
+		struct	if_settings ifru_settings;
+	} ifr_ifru;
+};
+#endif                                  // __UAPI_DEF_IF_IFREQ 
+ * 
+ * */
+static inline int get_mac_by_name(const char *device, unsigned char *buf)
+{
+	int sockfd, ret;
+	struct ifreq req;
+	sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (sockfd < 0) {
+		return 1;
+	}
+	snprintf(req.ifr_name, sizeof(req.ifr_name), "%s", device);
+	ret = ioctl(sockfd, SIOCGIFHWADDR, &req);
+	close(sockfd);
+	if (ret == -1) {
+		return 1;
+	}
+	memcpy(buf, req.ifr_hwaddr.sa_data, ETH_ALEN);
+	return 0;
+}
+
+/*判断字符串是不是一个合法ip*/
+static inline int str_is_ip(const char *str, int str_len)
+{
+    register int i, count;
+    register unsigned int val;
+
+    for (i = 0, count = 0, val = 0; i < str_len; i++) {
+        if (str[i] > '0' && str[i] <= '9') {
+            val = val * 10 + (str[i] - 0x30);
+            if (val > 255) {
+                return 0;
+            }
+        } else if (str[i] == '0') {
+            if (val == 0) {
+                if (i == str_len -1) {
+                    break;
+                } else if (str[i+1] != '.') {
+                    return 0;
+                }
+            } else {
+                val = val * 10 + (str[i] - 0x30);
+                if (val > 255) {
+                    return 0;
+                }
+            }
+        } else if (str[i] == '.') {
+            count++;
+            if (count > 3) {
+                return 0;
+            }
+            val = 0;
+        } else if (str[i] == ':') {
+            break;
+        } else {
+            return 0;
+        }
+    }
+    if (count == 3) {
+        return 1;
+    }
+
+    return 1;
+}
+
+
+
+unsigned long ip_get_mask(unsigned long addr)
+{
+  	unsigned long dst;
+
+  	if (addr == 0L) 
+  		return(0L);	/* special case */
+
+  	dst = ntohl(addr);
+  	if (IN_CLASSA(dst)) 
+  		return(htonl(IN_CLASSA_NET));
+  	if (IN_CLASSB(dst)) 
+  		return(htonl(IN_CLASSB_NET));
+  	if (IN_CLASSC(dst)) 
+  		return(htonl(IN_CLASSC_NET));
+  
+  	/*
+  	 *	Something else, probably a multicast. 
+  	 */
+  	 
+  	return(0);
+}
--- socket.h
+++ socket.h
@@ -2,8 +2,17 @@
 #define __SOCKET__
 
 
+#define	IN_CLASSA(a)		((((long int) (a)) & 0x80000000) == 0)
+#define	IN_CLASSA_NET		0xff000000
+#define	IN_CLASSB(a)		((((long int) (a)) & 0xc0000000) == 0x80000000)
+#define	IN_CLASSB_NET		0xffff0000
+#define	IN_CLASSC(a)		((((long int) (a)) & 0xe0000000) == 0xc0000000)
+#define	IN_CLASSC_NET		0xffffff00
 
 int Socket(const char *host, int clientPort);
+unsigned long ip_get_mask(unsigned long addr);
+static inline int get_mac_by_name(const char *device, unsigned char *buf);
+static inline int str_is_ip(const char *str, int str_len);
 
 #endif
 
