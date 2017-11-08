7ae706ebb02b8b268d7dab49e1ff0524310cc9a3
qigaohua <qigaohua168@163.com>
2017-11-08 04:04:42 -0800

    添加错误信号处理

---
+++ signal.c
@@ -0,0 +1,46 @@
+#include <execinfo.h>
+#include <signal.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/syscall.h>
+
+
+
+static void sigsegv_handler(int signal)
+{
+    char **strings;
+    size_t i;
+    size_t size;
+    uint32_t thread_id = syscall(__NR_gettid);
+    void *array[20];
+
+    syslog(LOG_ERR, "thread id %u sig num is %d \n", thread_id, signal);
+    size = backtrace (array, 20); 
+    strings = backtrace_symbols (array, size);
+    syslog(LOG_ERR, "Obtained %zd stack frames.\n", size);
+    for (i = 0; i < size; i++) {
+        syslog(LOG_ERR, "%s\n", strings[i]);
+    }    
+    ISMS_FREE (strings);
+    exit(1);
+}
+
+static inline void catch_sig_init(void)
+{
+    signal(SIGABRT, sigsegv_handler);
+    signal(SIGALRM, sigsegv_handler);
+    signal(SIGBUS, sigsegv_handler);
+    signal(SIGFPE, sigsegv_handler);
+    signal(SIGHUP, sigsegv_handler);
+    signal(SIGILL, sigsegv_handler);
+    signal(SIGKILL, sigsegv_handler);
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGQUIT, sigsegv_handler);
+    signal(SIGSEGV, sigsegv_handler);
+    signal(SIGTERM, sigsegv_handler);
+    signal(SIGTRAP, sigsegv_handler);
+    signal(SIGXCPU, sigsegv_handler);
+    signal(SIGXFSZ, sigsegv_handler);
+}
+
