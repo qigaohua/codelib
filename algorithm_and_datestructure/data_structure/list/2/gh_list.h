e7e46ac591b8625b82321c159f46bc0268255d61
qigaohua <qigaohua168@163.com>
2018-09-11 16:04:30 +0800

    fix

--- algorithm_and_datestructure/data_structure/list/2/gh_list.h
+++ algorithm_and_datestructure/data_structure/list/2/gh_list.h
@@ -3,14 +3,15 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <errno.h>
+#include <inttypes.h>
 
 
-typedef  signed char       int8_t;   
-typedef  unsigned char  uint8_t;  
-typedef  short          int16_t; 
-typedef  unsigned short uint16_t; 
-typedef  int            int32_t; 
-typedef  unsigned int   uint32_t;
+// typedef  signed char       int8_t;   
+// typedef  unsigned char  uint8_t;  
+// typedef  short          int16_t; 
+// typedef  unsigned short uint16_t; 
+// typedef  int            int32_t; 
+// typedef  unsigned int   uint32_t;
 
 typedef void (*func)(void*);
 
